e9467570b1e6a47d716dd813d52e2ba2f4a9919a
swordling <lachlanedwards@Lachlans-MacBook-Pro.local>
2015-01-17 14:53:28 +1000

    Updated, NO PHP

--- js/custom.js
+++ js/custom.js
@@ -54,3 +54,27 @@ $(window).scroll(function() {
     }
 
 }).scroll();
+
+$(function () {
+  "use strict";
+  // toggle password masking
+  $('.toggle-mask').on('click', function () {
+    var maskToggle = $(this).text();
+    var password = $('#password').val();
+    if (maskToggle === 'Show') {
+      $(this).text("Hide");
+      $('#password').replaceWith('<input type="text" id="password" name="password" maxlength="12" autocorrect="off" autocapitalize="off" value=' + password + '>');
+    } else if (maskToggle === 'Hide') {
+      $(this).text("Show");
+      $('#password').replaceWith('<input type="password" id="password" name="password" maxlength="12" autocorrect="off" autocapitalize="off" value=' + password + '>');
+    }
+  });
+
+  // highlight row on focus
+  $('#full-name, #email, #password').focus(function () {
+    $(this).parent().addClass('active');
+  });
+  $('#full-name, #email, #password').focusout(function () {
+    $('.active').removeClass('active');
+  });
+});
